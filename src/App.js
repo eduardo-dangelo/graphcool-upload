56c373ad287afda48a088d6a59d38c000e467091
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2018-05-30 23:17:40 +0100

    add graphcool

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone'
 
 class App extends Component {
+  state = {
+    imageId: null,
+    imageUrl: null
+  };
+
+  onDrop = (files) => {
+    let data = new FormData()
+    data.append('data', files[0])
+
+    // use the file endpoint
+    fetch('https://api.graph.cool/file/v1/cjhtn1d652lm7012050nzm91o', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      body: data
+    }).then(response => {
+      return response.json()
+    }).then(file => {
+      const fileId = file.id
+      console.log('file', file)
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +35,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={(e) => e.preventDefault}>
+          <h2>new form</h2>
+          {!this.state.imageId &&
+          <Dropzone
+            onDrop={this.onDrop}
+            accept='image/*'
+            multiple={false}
+          >
+            <div>Drop an image or click to choose</div>
+          </Dropzone>}
+          {this.state.imageUrl &&
+          <img src={this.state.imageUrl} role='presentation' className='w-100 mv3' />
+          }
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+// import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const client = new ApolloClient({
+  uri: "https://api.graph.cool/simple/v1/cjhtn1d652lm7012050nzm91o",
+  // cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
